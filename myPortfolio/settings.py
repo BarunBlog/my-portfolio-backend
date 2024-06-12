67688fc9a8a3816379c0128f01ee291fc6bdff86
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-06-12 21:46:24 +0600

    Updated richtext configuration

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -173,6 +173,21 @@ CLOUDINARY = {
 # For handling media files
 CLOUDINARY_URL = env('CLOUDINARY_URL')
 
+# TinyMCE Configuration
+TINYMCE_DEFAULT_CONFIG = {
+    "height": "500px",
+    "width": "960px",
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap print preview anchor searchreplace visualblocks code "
+    "fullscreen insertdatetime media table paste code help wordcount spellchecker",
+    "toolbar": "undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft "
+    "aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor "
+    "backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | "
+    "fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | "
+    "a11ycheck ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+}
+
 # security settings
 if ENVIRONMENT == 'production':
     SECURE_BROWSER_XSS_FILTER = True  # To help guard against XSS attacks
