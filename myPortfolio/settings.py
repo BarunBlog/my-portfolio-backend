e034d0783d837243c4a6f3c223d5b8e6873e556f
BarunBlog <bhattacharjeebarun25@gmail.com>
2024-07-11 21:45:39 +0600

    Updated the cors settings

--- myPortfolio/settings.py
+++ myPortfolio/settings.py
@@ -167,6 +167,7 @@ CORS_ORIGIN_WHITELIST = [
     'https://barun-bhattacharjee.web.app',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_HEADERS = ('content-type', 'accept', 'accept-encoding', 'authorization', 'Authorization',
                       'Custom-User-Agent', 'media_type', 'dnt', 'origin', 'user-agent', 'x-csrftoken',
